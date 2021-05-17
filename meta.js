96ed8b7db8a714ec6b0adf257e99492267d68db2
sky <shifeiyu@nbeport.com>
2021-05-17 18:02:52 +0800

    修改vue框架模版结构

---
+++ meta.js
@@ -0,0 +1,29 @@
+module.exports = {
+  metalsmith:{
+    before: addTestAnswers
+  },
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    },
+    template_version() {
+      return templateVersion
+    },
+  },
+
+  "prompts": {
+    "name": {
+      "type": "input",
+      "required": true,
+      "message": "项目名"
+    },
+    "author": {
+      "type": "input",
+      "message": "作者"
+    },
+  }
+}
